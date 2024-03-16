829c71924607f39976b6d087766d5b28ead3570b
dpark <davidparkus04@gmail.com>
2024-03-16 22:49:00 +1100

    my portfolio

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero"
+import About from "./About"
+import Tech from "./Tech"
+import Projects from "./Projects";
+import Experience from "./Experience"
+import Contact from "./Contact"
+import { BallCanvas } from './canvas';
+
+export { Navbar, Hero, About, Tech, Projects, Experience, Contact, BallCanvas }
